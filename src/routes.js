7517bf8802206fda0b9ceccbedf0a31351e8bbf2
navarrotheus <matheusnavarro@alu.ufc.br>
2020-02-07 07:13:02 -0300

    [~] routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import ExampleController from './app/controllers/ExampleController';
+
 const routes = new Router();
+
+routes.get('/examples', ExampleController.index);
+routes.get('/examples/:id', ExampleController.indexById);
 
 export default routes;
